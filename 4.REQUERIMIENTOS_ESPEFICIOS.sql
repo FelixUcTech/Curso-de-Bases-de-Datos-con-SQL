e2fc614bd67e29df4055173ff2c4e5e73e7ebc00
Félix Uc <fauh98@gmail.com>
2024-08-30 14:26:24 -0600

    Update 4.REQUERIMIENTOS_ESPEFICIOS.sql

--- 4.REQUERIMIENTOS_ESPEFICIOS.sql
+++ 4.REQUERIMIENTOS_ESPEFICIOS.sql
@@ -1,1 +1,7 @@
 --Esta práctica representa un requermiento de información de acuerdo a la base de datos que estamos trabajando.
+--De la clase de funciones Arimética Básicas en SQL (COUNT, SUM, AVG)
+SELECT COURSEID, COUNT(STUDENTID)
+FROM STUDENT_COURSE
+GROUP BY COURSEID
+
+
