7e59e0e683b525b909e8b1722bf259b940bcf8c0
Félix Uc <fauh98@gmail.com>
2024-08-31 17:56:24 -0600

    Update 6.JOINS.sql

--- 6.JOINS.sql
+++ 6.JOINS.sql
@@ -1,1 +1,9 @@
---
+--Los joins nos permiten relacionar dos o más tablas de ellas
+--Existen distintos tipos de joins:
+  --Inner Join
+--Nos trae 
+  --Left Join 
+  --Right Join
+  --Full Outer Join 
+  --Cross Join
+ 
