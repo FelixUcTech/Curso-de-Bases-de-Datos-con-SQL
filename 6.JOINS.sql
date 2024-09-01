752de2a9fdd557796f2187e2750f1c277f8d1168
Félix Uc <fauh98@gmail.com>
2024-09-01 14:33:11 -0600

    Update 6.JOINS.sql

--- 6.JOINS.sql
+++ 6.JOINS.sql
@@ -23,5 +23,9 @@ FROM students
 INNER JOIN enrollments ON students.student_id = enrollments.student_id
 INNER JOIN courses ON enrollments.course = courses.course;
 
-
 --Nos retorna únicamente la información que se encuentre presente en las dos tablas, y adicionalmente toda la información que se encunetre presente en la tabla que posicionaste al lado izquierdo de la consulta
+--Rigth join es lo mismo que el left join solo que la información que viene de manera total sería la tabla de la derecha, y las coicidencias serían de la izquierda
+--Características Clave del FULL OUTER JOIN:
+--Incluye todas las filas: Devuelve todas las filas de ambas tablas. Si no hay una coincidencia en una tabla, las filas de esa tabla aún se muestran en el resultado, con valores nulos en las columnas de la otra tabla.
+--Coincidencias y no coincidencias: Muestra tanto las filas coincidentes (donde las filas de ambas tablas cumplen la condición de la unión) como las no coincidentes (donde una fila de una tabla no tiene una coincidencia correspondiente en la otra tabla).
+--El cross join, es un 
