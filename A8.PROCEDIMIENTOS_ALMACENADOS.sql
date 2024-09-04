3668852b6a56b098a28e3745d44a110e9537dc90
Félix Uc <fauh98@gmail.com>
2024-09-04 09:44:57 -0600

    Update A8.PROCEDIMIENTOS_ALMACENADOS.sql

--- A8.PROCEDIMIENTOS_ALMACENADOS.sql
+++ A8.PROCEDIMIENTOS_ALMACENADOS.sql
@@ -1,1 +1,11 @@
-
+CREATE PROCEDURE IngresoEmpleado
+  @FirstName NVARCHAR(50),
+  @LastName NVARCHAR(50),
+  @DepartmentID INT,
+  @Salary Decimal(10,2),
+  @HireDate DATE
+AS
+BEGIN 
+      INSERT INTO Empreados(FirstName, LastName, DepartmentID, Salary, HireDate)
+      VALUES (@FirstName, @LastName, DepartmentID, Salary, HireDate);
+END;
