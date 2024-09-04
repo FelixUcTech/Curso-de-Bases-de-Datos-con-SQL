2022345aa15908fd2251e90769e2cfa222e4ea25
Félix Uc <fauh98@gmail.com>
2024-09-04 10:05:55 -0600

    Update A8.PROCEDIMIENTOS_ALMACENADOS.sql

--- A8.PROCEDIMIENTOS_ALMACENADOS.sql
+++ A8.PROCEDIMIENTOS_ALMACENADOS.sql
@@ -1,3 +1,4 @@
+--Sintaxis del procedimiento
 CREATE PROCEDURE IngresoEmpleado
   @FirstName NVARCHAR(50),
   @LastName NVARCHAR(50),
@@ -9,3 +10,9 @@ BEGIN
       INSERT INTO Empreados(FirstName, LastName, DepartmentID, Salary, HireDate)
       VALUES (@FirstName, @LastName, DepartmentID, Salary, HireDate);
 END;
+--Como se ejecuta la informaicón?, dependiendo el leguaje qué estemos utilizando
+--Opciión con CALL
+
+--Opción con EXEC
+
+
